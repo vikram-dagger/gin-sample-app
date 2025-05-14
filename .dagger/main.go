a5418f8b6e0d2a943bc08cce5251223f6705b09b
Vikram Vaswani <vikram@dagger.io>
2025-05-14 19:51:30 +0000

    Remove boilerplate functions
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- .dagger/main.go
+++ .dagger/main.go
@@ -63,18 +63,3 @@ func (m *Book) Env(
 		WithMountedDirectory("/app", source).
 		WithWorkdir("/app")
 }
-
-// Returns a container that echoes whatever string argument is provided
-func (m *Book) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
-
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *Book) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
