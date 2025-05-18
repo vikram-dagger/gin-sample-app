18ac03944738fb249405433359c339a077f22be6
Vikram Vaswani <vikram@dagger.io>
2025-05-18 16:03:02 +0000

    Updated code
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- .dagger/main.go
+++ .dagger/main.go
@@ -98,7 +98,7 @@ func (m *Book) UpdateChangelog(
 	fmt.Println("git status: ", out)
 
 	diff := ctr.
-		WithExec([]string{"sh", "-c", "git diff origin/main > /tmp/a.diff"}).
+		WithExec([]string{"sh", "-c", "git diff main > /tmp/a.diff"}).
 		File("/tmp/a.diff")
 
 	env := dag.Env(dagger.EnvOpts{Privileged: true}).
