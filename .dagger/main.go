c6e9b3d4df63442ecb78193d82df55ccb6248411
Vikram Vaswani <vikram@dagger.io>
2025-05-18 20:13:32 +0000

    Updated workflow
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- .dagger/main.go
+++ .dagger/main.go
@@ -83,7 +83,7 @@ func (m *Book) UpdateChangelog(
 		WithWorkdir("/app")
 
 	diff := ctr.
-		WithExec([]string{"git", "fetch", "origin", "main"}).
+		//WithExec([]string{"git", "fetch", "origin", "main"}).
 		WithExec([]string{"sh", "-c", "git diff origin/main > /tmp/a.diff"}).
 		File("/tmp/a.diff")
 
